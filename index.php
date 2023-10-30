434df987ad99d0795fbf7ac8b2f82e0580bcfe3f
vrushabh-vb <vrushabbhabire3041@gmail.com>
2023-10-30 17:40:08 +0530

    last

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ session_start();
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -41,6 +42,9 @@ session_start();
       </div>
       <div class="carousel-item">
         <img src="./img/slide4.webp" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="./img/slide5.webp" class="d-block w-100" alt="..." />
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
